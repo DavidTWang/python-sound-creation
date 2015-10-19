497a8c0dba7bbfdfd09083b87daedbe24d49bf58
Hakus <altenotiz@gmail.com>
2015-10-18 23:46:22 -0700

    Finished one song

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Requirements:
 PyDub https://github.com/jiaaro/pydub
 
 Pydub can be installed with ```pip install pydub```
+
+This is a PoC using PyDub to create music
+The current implementation of this program only creates a set song.
+All the mixes and timings are hard-coded.
+
+
+To run the program, simply run ```python creator.py```
--- creator.py
+++ creator.py
@@ -1,26 +1,67 @@
-from pydub import *
+from pydub import AudioSegment
 
 
 def main():
+    song = AudioSegment.from_mp3("sounds/sax.mp3")
+    ol = 7324  # Length of 1 loop
+    song = song + 5  # Increase volume
+    result = []  # Segment array
+    output = None
 
-song = AudioSegment.from_mp3("epicsaxguy.mp3")
+    intro_piano = AudioSegment.from_file("sounds/intro_piano.wav")
+    intro_beats = AudioSegment.from_file("sounds/intro_beats.wav")
+    are_you_ready = AudioSegment.from_file("sounds/are you ready.wav")
+    static = AudioSegment.from_file("sounds/static.mp3")
+    # Combine the intros
+    intro = intro_piano + intro_beats
+    # Overlay "Are you ready"
+    intro = intro.overlay(are_you_ready, position=-5300)
+    # Interrupt with static
+    output = intro + (static[750:] - 10)
 
-chunk1 = song[:30000]
-reverse = chunk1.reverse()
+    # Loop the song twice
+    two_loops = song[:ol*3]
+    result.append(two_loops)
 
-start = chunk1.append(reverse, crossfade=5000)
+    # Glitch sound
+    glitch = AudioSegment.from_file("sounds/glitch.mp3")
+    result.append(glitch)
 
-chunk2 = song[31000:60000]
-decVolume = reverse -3
-blender = popin.overlay(chunk2)
+    # Reverse the song
+    reverse = song[:ol*4].reverse()
+    result.append(reverse)
 
-chunk3 = song[60000:90000]
-speed = chunk3.speedup(2)
-middle = blender.append(speed, crossfade = 5000)
+    # Speed up the reverse
+    speed_reverse = reverse[:ol*2].speedup(2.0)
+    result.append(speed_reverse)
 
+    # Play a glitched sound 10 times
+    loop2 = speed_reverse[-130:] * 10
+    result.append(loop2)
 
-complete = start + middle
-complete.export ("test.mp3", format="mp3", bitrate="192k")
+    # Some silence
+    silence = AudioSegment.silent(duration=1500)
+    result.append(silence)
+
+    # Fade in the original song
+    fade_in = song[ol:ol*3+150].fade_in(ol)
+    result.append(fade_in)
+
+    # Add drums to it
+    drum_overlay = AudioSegment.from_file("sounds/drum.wav") * 15
+    drum_overlay = drum_overlay.fade_in(ol)
+    drum_overlay = song[:ol*3].overlay(drum_overlay)
+    result.append(drum_overlay)
+
+    # Outro. Suddenly static
+    static_fade = ((static - 9) * 5).fade_out(5000)
+    outro = (static[:250] - 5) + silence[:100] + (static[:150] - 9) + silence[:500] + static_fade
+    result.append(outro)
+
+    # Add all the segments together, export
+    for segments in result:
+        output += segments
+    output.export("output.mp3", format="mp3")
 
 
 if __name__ == '__main__':
